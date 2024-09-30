cf14850ee78dea8240b2cfd25aa634663c259238
Yash Chapani <yashchapani.57@gmail.com>
2024-09-30 23:39:46 +0530

    Implemented Template Preview Section

--- src/app/components/pages/template/template.component.ts
+++ src/app/components/pages/template/template.component.ts
@@ -1,14 +1,94 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatStepper, MatStepperModule } from '@angular/material/stepper';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {
+  AngularEditorConfig,
+  AngularEditorModule,
+} from '@kolkov/angular-editor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [MatStepperModule],
+  imports: [
+    MatStepperModule,
+    NgSelectModule,
+    AngularEditorModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DatePipe
+  ],
   templateUrl: './template.component.html',
-  styleUrl: './template.component.scss'
+  styleUrl: './template.component.scss',
 })
 export class TemplateComponent {
   @ViewChild('stepper') stepper!: MatStepper;
+  headerText: string | null = null;
+  footerText: string | null = null;
+  bodyContent: string | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
+  currentTime: Date = new Date();
 
+
+  headerContent: any[] = [
+    { id: 1, name: 'None', value: '' },
+    { id: 2, name: 'Text', value: 'text' },
+    { id: 3, name: 'Image', value: 'image' },
+  ];
+
+  selectedHeaderType: string = '';
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    toolbarHiddenButtons: [
+      [
+        'undo',
+        'redo',
+        'subscript',
+        'superscript',
+        'justifyLeft',
+        'justifyCenter',
+        'justifyRight',
+        'justifyFull',
+        'indent',
+        'outdent',
+        'insertUnorderedList',
+        'insertOrderedList',
+        'heading',
+        'fontName',
+      ],
+      [
+        'fontSize',
+        'textColor',
+        'backgroundColor',
+        'customClasses',
+        'link',
+        'unlink',
+        'insertImage',
+        'insertVideo',
+        'insertHorizontalRule',
+        'removeFormat',
+        'toggleEditorMode',
+      ],
+    ],
+  };
+
+  onFileSelected(event: Event): void {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files && fileInput.files[0]) {
+      const file = fileInput.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imagePreview = reader.result;
+      };
+
+      reader.readAsDataURL(file); // Convert image to base64
+    }
+  }
 }
